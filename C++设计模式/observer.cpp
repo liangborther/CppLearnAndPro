f306a1dabe42566cd2e949f48e3172eceffa70dc
LL2002 <1007585047@qq.com>
2024-08-08 22:11:25 +0800

    更新设计模式

--- C++设计模式/observer.cpp
+++ C++设计模式/observer.cpp
@@ -13,4 +13,62 @@ public:
 class ISubject {
 public:
     virtual ~ISubject(){}
+    virtual void attach(shared_ptr<IObserver> observer) = 0;
+    virtual void detach(shared_ptr<IObserver> observer) = 0;
+    virtual void notify() = 0;
 };
+class subject : public ISubject{
+private:
+    list<shared_ptr<IObserver>> mListObservers;
+    string mmessage;
+public:
+    virtual ~subject(){}
+    void attach(shared_ptr<IObserver> observer) override{
+        mListObservers.push_back(observer);
+    }
+    void detach(shared_ptr<IObserver> observer) override{
+        mListObservers.remove(observer);
+    }
+    void notify() override{
+        for (auto &observer : mListObservers)
+        {
+            observer->update(mmessage);
+        }
+    }
+    void createMessage(const string& message){
+        mmessage = message;
+        notify();
+    }
+};
+class observer : public IObserver{
+private:
+    string mMessageFromSubject;
+    shared_ptr<ISubject> mSubject;
+    static int mStaticNumber;
+    int mNumber;
+public:
+    observer(shared_ptr<ISubject> subject) : mSubject(subject){
+        this->mNumber = mStaticNumber;
+        mSubject->attach(make_shared<observer>(*this));
+    }
+    virtual ~observer(){}
+    void update(const string& message_from_subject) override{
+        mMessageFromSubject = message_from_subject;
+        cout<< "observer "<<mNumber<<" get message "<<mMessageFromSubject<<endl;
+    }
+    void removeMeFromTheList(){
+        mSubject.get()->detach(make_shared<observer>(*this));
+    }
+};
+int observer::mStaticNumber = 2;
+
+void clientCode() {
+    shared_ptr<subject> rSubject = shared_ptr<subject>(new subject());
+    shared_ptr<observer> rObserver1 = shared_ptr<observer>(new observer(rSubject));
+    shared_ptr<observer> rObserver2 = shared_ptr<observer>(new observer(rSubject));
+    rSubject.get()->createMessage("hello");
+    rObserver1.get()->removeMeFromTheList();
+}
+int main(){
+    clientCode();
+}
---
+++ C++设计模式/strategy.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+using namespace std;
+class strategy{
+public:
+virtual ~strategy(){}
+virtual string doAlgorithm(const vector<string> &v) = 0;
+};
+class context{
+private:
+shared_ptr<strategy> mStrategy;
+public:
+context(shared_ptr<strategy> strategy) : mStrategy(strategy){}
+~context(){}
+void setStrategy(shared_ptr<strategy> strategy){
+    mStrategy.reset();
+    this->mStrategy = strategy;
+}
+void doSomeBusinessLogic() const{
+    cout<<mStrategy.get()->doAlgorithm({"x","b","c"})<<endl;
+}
+};
+class concreteStrategyA : public strategy{
+public:
+    string doAlgorithm(const vector<string>& v) override{
+        string res ="";
+        for_each(v.begin(),v.end(),[&res](const string& s) {res += s;});
+        return res;
+    }
+};
+class concreteStrategyB : public strategy{
+public:
+    string doAlgorithm(const vector<string>& v) override{
+        string res ="";
+        for_each(v.begin(),v.end(),[&res](const string& s) {res += s;});
+        sort(res.begin(),res.end());
+        return res;
+    }
+};
+
+void clientCode(){
+    shared_ptr<strategy> strategyA = make_shared<concreteStrategyA>();
+    shared_ptr<strategy> strategyB = make_shared<concreteStrategyB>();
+    shared_ptr<context> contextA = make_shared<context>(strategyA);
+    shared_ptr<context> contextB = make_shared<context>(strategyB);
+    contextA.get()->doSomeBusinessLogic();
+    contextB.get()->doSomeBusinessLogic();
+}
+int main(){
+    clientCode();
+}
